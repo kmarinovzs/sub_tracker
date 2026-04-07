0e18852bcbcaf7da7e1766b4317969739d7aeebe
Kaloyan Marinov <k.marinov.dev@gmail.com>
2026-04-07 20:59:59 +0200

    backup

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, jsonify, render_template, redirect, flash, session, request, flash, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User, Subscription
+from .models import User, Subscription, db
 
 app_bp = Blueprint('main', __name__, template_folder="templates")
         
 
-
 @app_bp.route("/", methods=["GET"])
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
 
 
 @app_bp.route("/register", methods=["GET", "POST"])
@@ -22,7 +21,7 @@ def register():
         if not username or not password or not confirmation:
             flash("Fill all the fields")
             return redirect(url_for("main.register"))
-        
+
         if password != confirmation:
             flash("Passwords do not match")
             return redirect(url_for("main.register"))
@@ -34,7 +33,7 @@ def register():
             return redirect("/")
         except Exception as e:
             db.session.rollback()
-            flash("Username may already exist")
+            flash("The username already exists")
             print(f"ERROR: {e}")
             return redirect(url_for("main.register"))
     
@@ -43,5 +42,30 @@ def register():
 
 @app_bp.route("/login", methods = ["GET", "POST"])
 def login():
+
     if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            flash("Enter your username and password correctly!")
+            return redirect(url_for("main.login"))
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.hash, password):
+            session["user_id"] = user.id
+            session["username"] = user.username
+            return redirect(url_for("main.index"))
+        else:
+            flash("Username or password are not correct")
+            return redirect(url_for("main.login"))
         
+    return render_template("login.html")
+            
+
+@app_bp.route("/logout", methods = ["POST"])
+def logout():
+
+    session.clear()
+    return redirect(url_for("main.index"))
+
